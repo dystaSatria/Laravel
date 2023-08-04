0a28881bef2b92175c57f03b0a2573cd59171895
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-08-04 18:58:20 +0300

    Add files via upload

---
+++ readyLaravelSplade/splade/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        
+            $this->call([
+            
+                ClassesSeeder::class,
+                UsersTableSeeder::class,
+                PermissionsTableSeeder::class,
+                RolesTableSeeder::class,
+                PermissionRoleTableSeeder::class,
+                RoleUserTableSeeder::class,
+
+
+            ]);
+
+
+    }
+}
